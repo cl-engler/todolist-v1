0680497f2ee4c75cf8abc67e427e02e971529372
Curtis <curtis_engler@protonmail.ch>
2022-01-08 16:43:23 -0800

    fixed coding errors and added procfile

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.post("/", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on 3000.");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running.");
 });
